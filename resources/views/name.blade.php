90fb5186d06725ec1bde2cfcc5205507ae0c0073
chaiqou <lomtadzenikusha@gmail.com>
2022-04-12 00:30:50 +0400

    Fix bad css code

--- resources/views/name.blade.php
+++ resources/views/name.blade.php
@@ -6,14 +6,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/name.css"/>
+    <link rel="stylesheet" href="/app.css"/>
     <title>Hello <?php echo $name; ?></title>
 </head>
 <body>
-    <div class="container1">
-        <div class="robot-photo1" />
-        <div class="hello1">< <p class='blue1'>Hello, </p> <p class="red1"><?php echo $name; ?>!</p> /></div>
+    <div class="container">
+      <img class="robot-photo" src="/robot.png" />
+      <div class="hello-text">< <p class='first-word'>Hello, </p> <p class="second-word"><?php echo $name; ?>!</p> /></div>
     </div>
     
 </body>
 </html>
+
+<!-- <div class="robot-photo1" /> -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,13 +6,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/welcome.css"/>
+    <link rel="stylesheet" href="/app.css"/>
     <title>Hello World</title>
 </head>
 <body>
     <div class="container">
         <img class="robot-photo" src="/robot.png" />
-        <div class="hello">< <p class='blue'>Hello </p> <p class="red"> World</p> /></div>
+        <div class="hello-text">< <p class='first-word'>Hello </p> <p class="second-word"> World</p> /></div>
     </div>
     
 </body>
