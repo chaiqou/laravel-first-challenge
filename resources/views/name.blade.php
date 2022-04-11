cf0d76cc61974b274c6f33c8df3c0e74130f87be
chaiqou <lomtadzenikusha@gmail.com>
2022-04-12 00:15:57 +0400

    get random url name from path and print , fix css bugs

---
+++ resources/views/name.blade.php
@@ -0,0 +1,19 @@
+
+  
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/name.css"/>
+    <title>Hello <?php echo $name; ?></title>
+</head>
+<body>
+    <div class="container1">
+        <div class="robot-photo1" />
+        <div class="hello1">< <p class='blue1'>Hello, </p> <p class="red1"><?php echo $name; ?>!</p> /></div>
+    </div>
+    
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,19 @@
+
+  
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/app.css"/>
+    <link rel="stylesheet" href="/welcome.css"/>
     <title>Hello World</title>
 </head>
 <body>
     <div class="container">
-        <div class="robot-photo" />
+        <img class="robot-photo" src="/robot.png" />
         <div class="hello">< <p class='blue'>Hello </p> <p class="red"> World</p> /></div>
     </div>
     
 </body>
 </html>
-
